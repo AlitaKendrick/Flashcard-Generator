12781dc34459def3106cf957d1938d8335ad3211
alita kendrick <alitakendrick@Alitas-MacBook-Pro.local>
2017-04-13 20:20:20 -0500

    complete

--- app.js
+++ app.js
@@ -1,36 +1,31 @@
+//variables to grab flashcard info from json files
 var clozeQuestions = require("./cloze.json");
 var basicQuestions = require("./basic.json");
 var inquirer = require("inquirer");
 var fs = require("fs");
 var count = 0;
 
+//function to begin the game with a prompt to select type of flashcards 
+function begin() {
+	inquirer.prompt([
+			{
+			name: "begin",
+			type: "list",
+			message: "Select the type of flashcards you would like.",
+			choices: ["basic flashcards", "cloze flashcards"]
+			} 
+		]).then(function(answer) {
+			if(answer.begin === "basic flashcards") {
+				basicFlashcard();
+			} else {
+				clozeFlashcard();
+			}
+		}); // closes promise
+}; //closes function
+begin();
 
-//function for basic cards to accept front and back arguments
-function basicCards (front, back) {
-	this.front = front,
-	this.back = back
-};
 
-function clozeCards (text, cloze) {
-	this.text = text,
-	this.cloze = cloze,
-	this.full = this.text + " " + this.cloze,
-	this.clozeReturn = function() {
-		return this.cloze;
-	},
-	this.clozePartial = function() {
-		return this.text;
-	},
-	this.clozeFull = function() {
-		return this.full;
-	},
-	this.checkError = function() {
-		if (this.full.includes(this.cloze)===false){
-			throw error;
-		};
-	}
-};
-
+//function to ask if you want to restart the game once all questions have been answered in a set
 function restart () {
 	inquirer.prompt([
 		{
@@ -40,14 +35,17 @@ function restart () {
 		}
 		]).then(function(answers) {
 			if (answers.restart === true) {
-				basicCards();
+				console.log("------------------------------------");
+				begin();
 			} else {
 				console.log("PEACE OUT!");
 				process.exit();
 			}
-		});
-};
+		}); //close promise
+}; // close function
 
+
+//if user selects basic flashcards this function will run and go through all questions
 var basicFlashcard = function() {
 	if (count < basicQuestions.length) {
 		inquirer.prompt([
@@ -58,15 +56,49 @@ var basicFlashcard = function() {
 			}
 		]).then(function(answers) {
 			if (answers.basic === basicQuestions[count].back) {
-				console.log("correct!!");
+				console.log("Correct!!");
+				console.log("------------------------------------");
 			} else {
-				console.log("wrong ):");
+				console.log("Wrong. The correct answer was " + basicQuestions[count].back);
+				console.log("------------------------------------");
 			};
 			count++;
 			basicFlashcard();
-		});
+		}); //close promise
 	} else {
+		console.log("------------------------------------");
 		console.log("you finished all of the basic cards!");
-	};
-};
-basicFlashcard();
+		restart();
+	}; //closes if/else statement
+}; // closes function
+
+// basicFlashcard();
+
+
+//if user selects cloze flashcards this function will run and go through all questions
+var clozeFlashcard = function() {
+	if (count < clozeQuestions.length) {
+		inquirer.prompt([
+			{
+				name: "cloze",
+				type: "input",
+				message: clozeQuestions[count].front
+			}
+		]).then(function(answers) {
+			if (answers.cloze === clozeQuestions[count].cloze) {
+				console.log("Correct!!");
+				console.log("------------------------------------");
+			} else {
+				console.log("Wrong! The correct answer was " + clozeQuestions[count].back);
+				console.log("------------------------------------");
+			}; //closes promise if/else statement
+			count++;
+			clozeFlashcard();
+		}); //closes promise
+	} else {
+		console.log("You have finished all of the cloze cards!");
+		restart();
+	}; // closes if/else statement
+}; //closes cloze function
+
+// clozeFlashcard();
